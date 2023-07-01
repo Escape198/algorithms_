b7a1310db70e8803d280146346d6e49e2b9a18f0
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-07-02 00:22:28 +0700

    Update search_unique.py

--- search_unique.py
+++ search_unique.py
@@ -1,7 +1,24 @@
-nums = list(range(1, 15**6))
+from memory_profiler import profile
+
+import time
+
+
+nums = list(range(1, 30**5))
 nums += nums
 
 
+def timer(func):
+    def wrapper(*args, **kwargs):
+        before = time.monotonic()
+        retval = func(*args, **kwargs)
+        after = time.monotonic() - before
+        print("Function {}: {} seconds".format(func.__name__, after))
+        return retval
+    return wrapper
+
+
+@profile
+@timer
 def search_num(numbers: list) -> int:
     import operator
     import functools
