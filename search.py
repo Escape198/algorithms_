0cb4879383d0f28c46fb5d4c9cd851e532e961b7
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-07-01 12:57:54 +0700

    Create search.py

---
+++ search.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+
+numbers = list(range(1, 30**5))
+k = len(numbers)
+num = random.randint(1, 30**5)
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        before = time.monotonic()
+        retval = func(*args, **kwargs)
+        after = time.monotonic() - before
+        print("Function {}: {} seconds".format(func.__name__, after))
+        return retval
+    return wrapper
+
+
+@timer
+def binary_search(arr, x):
+    left = 0
+    right = k-1
+    while right > left + 1:
+        middle = (left + right) // 2
+        if arr[middle] > x:
+            right = middle
+        else:
+            left = middle
+    return right
+
+
+print(binary_search(numbers, num))
