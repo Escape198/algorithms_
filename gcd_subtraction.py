ab78a7cb98225f938386ab2d657e6b751eafd2e0
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-07-02 11:36:02 +0700

    Update gcd_subtraction.py

--- gcd_subtraction.py
+++ gcd_subtraction.py
@@ -33,5 +33,30 @@ def gcd_extended(num1: int, num2: int) -> tuple:
     return div, y - (num2 // num1) * x, x
 
 
-print(gcd_rem_division(320, 3600))
-print(gcd_extended(320, 3600))
+@profile
+@timer
+def binary_gcd(num1: int, num2: int) -> int:
+    shift = 0
+    if num1 == 0:
+        return num2
+    if num2 == 0:
+        return num1
+    while (num1 | num2) & 1 == 0:
+        shift += 1
+        num1 >>= 1
+        num2 >>= 1
+    while num1 & 1 == 0:
+        num1 >>= 1
+    while num2 != 0:
+        while num2 & 1 == 0:
+            num2 >>= 1
+        if num1 > num2:
+            num1, num2 = num2, num1
+        num2 -= num1
+    return num1 << shift
+
+
+x, y = 32032, 360000144
+print(gcd_rem_division(x, y))
+print(gcd_extended(x, y))
+print(binary_gcd(x, y))
