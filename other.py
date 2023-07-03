3f3d403c07b418a3e1a7cce2bdac21abd906041f
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-07-03 11:40:45 +0700

    Update other.py

--- other.py
+++ other.py
@@ -8,3 +8,10 @@ def reverse_bits(n: str) -> int:
     oribin = '{0:032b}'.format(n)
     reversebin = oribin[::-1]
     return int(reversebin, 2)
+
+def hamming_weight(n: str) -> int:
+        cnt = 0
+        while n :
+            cnt += 1
+            n = n & (n-1)
+        return cnt
