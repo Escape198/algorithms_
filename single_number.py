732d695ee8e97e490ad0a8d716c224326d908363
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2023-07-04 20:17:56 +0700

    Update single_number.py

--- single_number.py
+++ single_number.py
@@ -1,4 +1,20 @@
-def single_number(self, nums: list) -> str:
+from memory_profiler import profile
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        before = time.monotonic()
+        retval = func(*args, **kwargs)
+        after = time.monotonic() - before
+        print(f"Function {func.__name__}: {after} seconds")
+        return retval
+    return wrapper
+
+
+@profile
+@timer
+def single_number(self, nums: list) -> int:
         ones = 0
         twos = 0
         for num in nums:
